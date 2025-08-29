0d38da96e2b84fa255362a77c3f9c6de075d5083
Sudhanshu singh <sudhanshu6073@gmail.com>
2025-08-29 09:52:54 +0530

    medium_done

--- medium-blog/frontend/src/pages/Blog.tsx
+++ medium-blog/frontend/src/pages/Blog.tsx
@@ -1,8 +1,31 @@
 import { useParams } from "react-router-dom"
+import { useBlog } from "../hooks";
+import { AppBar } from "../components/Appbar";
+import { Spinner } from "../components/Spinner";
+import { FullBlog } from "../components/FullBlog";
 
 
 
 
-export const Blogs = () => {
+export const Blog = () => {
     const {id} = useParams();
+    const {loading,blog} = useBlog({
+        id: id || ""
+    });
+
+    if(loading || !blog) {
+        return <div>
+            <AppBar/>
+            <div className="h-screen flex flex-col justify-center">
+                
+                <div className="flex justify-center">
+                    <Spinner />
+                </div>
+            </div>
+        </div>
+    }
+
+    return <div>
+        <FullBlog blog={blog} />
+    </div>
 }
